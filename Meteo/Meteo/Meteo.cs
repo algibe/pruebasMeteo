8c5a961898030608d927b734cd3c878f0f9bc359
algibe <albertgb@hotmail.es>
2016-04-06 21:44:35 +0200

    MeteoRoger
    MeteoRoger

---
+++ Meteo/Meteo/Meteo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoNamespace
+{
+    public class Meteo
+    {
+        public List<int> temperatures { get; set; }
+        public List<int> pluges { get; set; }
+        public List<int> vent { get; set; }
+
+
+        public Meteo()
+        {
+            this.temperatures = new List<int>();
+            this.pluges = new List<int>();
+            this.vent = new List<int>();
+        }
+
+        /// <summary>
+        /// Returns max temp 
+        /// </summary>
+        /// <returns></returns>
+        public int MaxTemp()
+        {
+            int Max = int.MinValue;
+
+            foreach (int T in temperatures)
+            {
+                if (T > Max)
+                {
+                    Max = T;
+                }
+            }
+            return Max;
+        }
+
+        public bool alerta_incendi (int temp_mitja, int pluja_total, bool vent_fort)
+        {
+            if (temp_mitja > 25 && pluja_total < 10 && vent_fort == true)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }                    
+        }
+    }
+}
---
+++ MeteoTest/MeteoTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MeteoNamespace;
+
+namespace MeteoTest
+{
+    [TestClass]
+    public class MeteoTest
+    {
+        [TestMethod]
+        public void TestMaxTemp()
+        {
+            //Arrange
+            Meteo MyMeteo = new Meteo();
+
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(8);
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(1);
+            MyMeteo.temperatures.Add(1);
+            
+            //Act
+            int Max = MyMeteo.MaxTemp();
+
+            //Assert
+            Assert.AreEqual(8, Max);
+        }
+
+        [TestMethod]
+        public void Prova1()
+        {
+            //Arrange
+            Meteo MyMeteo = new Meteo();
+            int temp = 30;
+            int pluja = 5;
+            bool vent_fort = true;
+
+            //Act
+            bool Max = MyMeteo.alerta_incendi(temp, pluja, vent_fort);
+
+            //Assert
+            Assert.AreEqual(true, Max);
+        }
+
+        [TestMethod]
+        public void Prova2()
+        {
+            //Arrange
+            Meteo MyMeteo = new Meteo();
+            int temp = 30;
+            int pluja = 5;
+            bool vent_fort = false;
+
+            //Act
+            bool Max = MyMeteo.alerta_incendi(temp, pluja, vent_fort);
+
+            //Assert
+            Assert.AreEqual(false, Max);
+        }
+
+        [TestMethod]
+        public void Prova3()
+        {
+            //Arrange
+            Meteo MyMeteo = new Meteo();
+            int temp = 30;
+            int pluja = 12;
+            bool vent_fort = true;
+
+            //Act
+            bool Max = MyMeteo.alerta_incendi(temp, pluja, vent_fort);
+
+            //Assert
+            Assert.AreEqual(false, Max);
+        }
+
+        [TestMethod]
+        public void Prova4()
+        {
+            //Arrange
+            Meteo MyMeteo = new Meteo();
+            int temp = 30;
+            int pluja = 12;
+            bool vent_fort = false;
+
+            //Act
+            bool Max = MyMeteo.alerta_incendi(temp, pluja, vent_fort);
+
+            //Assert
+            Assert.AreEqual(false, Max);
+        }
+
+
+
+
+    }
+}
